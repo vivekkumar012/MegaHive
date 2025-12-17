3a4fa98c90b2f3172a9e6e88c88e20dafd06f8be
Vivek Kumar <vh118854@gmail.com>
2025-12-18 01:28:34 +0530

    updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import MyOrdersPage from "./MyOrdersPage";
 
 const Profile = () => {
   return (
-    <div>
-      Profile Page
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow container mx-auto p-4 md:p-6">
+        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 ">
+          {/* Left Section */}
+          <div className="w-full md:w-1/3 lg:w-1/4 shadow-md rounded-lg p-6">
+            <h1 className="text-2xl md:text-3xl font-bold mb-4">John Doe</h1>
+            <p className="text-lg text-gray-600 mb-4">John@gmail.com</p>
+            <button className="text-white bg-red-500 w-full py-2 px-4 rounded hover:bg-red-600">
+              Logout
+            </button>
+          </div>
+          {/* Right Section */}
+          <div className="w-full md:w-2/3 lg:w-3/4">
+            <MyOrdersPage />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
