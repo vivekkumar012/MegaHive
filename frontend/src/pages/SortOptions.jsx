092a63d16ac326045b619568cd3f12e0c91c0765
Vivek Kumar <vh118854@gmail.com>
2025-12-31 18:28:43 +0530

    new codes added

--- frontend/src/pages/SortOptions.jsx
+++ frontend/src/pages/SortOptions.jsx
@@ -1,78 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import React from 'react'
 
 const SortOptions = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [filters, setFilters] = useState({
-    category: "",
-    gender: "",
-    color: "",
-    size: [],
-    material: [],
-    brand: [],
-    minPrice: 0,
-    maxPrice: 100,
-  });
-  const [priceRange, setPriceRange] = useState([0, 100]);
+  return (
+    <div>
+      SortOptions
+    </div>
+  )
+}
 
-  const categories = ["Top Wear", "Bottom Wear"];
-
-  const colors = [
-    "Red",
-    "Black",
-    "Blue",
-    "Green",
-    "Yellow",
-    "Gray",
-    "White",
-    "Pink",
-    "Beige",
-    "Navy",
-  ];
-
-  const sizes = ["XS", "S", "M", "L", "XL", "XXl"];
-
-  const material = [
-    "Cotton",
-    "Wool",
-    "Denim",
-    "Polyster",
-    "Silk",
-    "Linene",
-    "Viscose",
-    "Fleece",
-  ];
-
-  const brands = [
-    "Urben Threads",
-    "Modern Fit",
-    "Street Style",
-    "Beach Breeze",
-    "Fashionista",
-    "ChicStyle",
-  ];
-
-  const genders = ["Men", "Women"];
-
-  useEffect(() => {
-    const params = Object.fromEntries([...searchParams]);
-
-    setFilters({
-      category: params.category || "",
-      gender: params.gender || "",
-      color: params.color || "",
-      size: params.size ? params.size.split(",") : [],
-      material: params.material ? params.material.split(",") : [],
-      brand: params.brand ? params.brand.split(",") : [],
-      minPrice: params.minPrice || 0,
-      maxPrice: params.maxPrice || 100,
-    });
-    setPriceRange([0, params.maxPrice || 100]);
-  }, [searchParams]);
-
-  return <div className="p-4">
-    <h3 className="text-xl font-medium text-gray-800 mb-4"></h3>
-  </div>;
-};
-
-export default SortOptions;
+export default SortOptions
