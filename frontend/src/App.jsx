c3c0160f5bee8ac2166946ee6c108f28076670de
Vivek Kumar <vh118854@gmail.com>
2026-01-06 21:06:20 +0530

    added new comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
 
 function App() {
   return (
@@ -41,6 +43,8 @@ function App() {
 
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
         </Route>
       </Routes>
     </div>
