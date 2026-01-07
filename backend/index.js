be4d5ffbb0823d21004167468c4a1a08790235da
Vivek Kumar <vh118854@gmail.com>
2026-01-07 17:28:12 +0530

    backend start

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express"
+import cors from "cors";
+import dotenv from "dotenv"
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use("/api/v1/user", );
+dotenv.config();
 
-const port = 3001;
+const port = 3001 || process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Backend of MEGAHIVE")
+})
+
 app.listen(port, () => {
     console.log(`App is Listening on PORT${port}`);
 })
