a3222f91056e6799fcb903d0d950b89e1e0b857b
Vivek Kumar <vh118854@gmail.com>
2026-01-11 14:46:16 +0530

    product updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -109,3 +109,166 @@ export const deleteProduct = async (req, res) => {
         })
     }
 }
+
+export const getQueryProduct = async (req, res) => {
+    try {
+        const { collection, size, color, gender, minPrice, maxPrice, sortBy,
+            search, category, material, brand, limit
+        } = req.query;
+
+        let query = {};
+
+        //Filter Logic
+        if (collection && collection.toLocaleLowerCase() !== 'all') {
+            query.collection = collection;
+        }
+        if (category && category.toLocaleLowerCase() !== 'all') {
+            query.category = category;
+        }
+
+        if (material) {
+            query.material = { $in: material.split(",") };
+        }
+
+        if (brand) {
+            query.brand = { $in: brand.split(",") };
+        }
+
+        if (size) {
+            query.sizes = { $in: size.split(",") };
+        }
+
+        if (color) {
+            query.colors = { $in: [color] };
+        }
+
+        if (gender) {
+            query.gender = gender;
+        }
+
+        if (minPrice || maxPrice) {
+            query.price = {};
+            if (minPrice) {
+                query.price.$gte = Number(minPrice);
+            }
+            if (maxPrice) query.price.$lte = Number(maxPrice)
+        }
+
+        if (search) {
+            query.$or = [
+                { name: { $regex: search, $options: "i" } },
+                { description: { $regex: search, $options: "i" } },
+            ]
+        }
+
+        let sort = {}
+        if (sortBy) {
+            switch (sortBy) {
+                case "priceAsc":
+                    sort = { price: 1 };
+                    break;
+                case "priceDesc":
+                    sort = { price: -1 };
+                    break;
+                case "popularity":
+                    sort = { rating: -1 }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //Fetch Product and apply sorting and limit
+        let products = await productModel.find(query).sort(sort).limit(Number(limit) || 0);
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({
+            message: "Error in query access",
+            error: error.message
+        })
+    }
+}
+
+export const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid product ID" });
+        }
+        const product = await productModel.findById(id);
+        if (!product) {
+            return res.status(402).json({
+                message: "Product not found!"
+            })
+        }
+        return res.json({
+            product
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(502).json({
+            message: "Server Error in fetching product",
+            error: error.message
+        })
+    }
+}
+
+export const getCategoryProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await productModel.findById(id);
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            })
+        }
+
+        const similarProducts = await productModel.find({
+            _id: { $ne: id }, //Exclude the curr product id
+            gender: product.gender,
+            category: product.category
+        }).limit(4);
+
+        res.json(similarProducts);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server Error",
+            error: error.message
+        })
+    }
+}
+
+export const getHigherRatedProduct = async (req, res) => {
+    try {
+        const bestSeller = await productModel.findOne().sort({ rating: -1 });
+        if (bestSeller) {
+            res.json(bestSeller)
+        } else {
+            res.status(404).json({
+                message: "No best seller found"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server Error",
+            error: error.message
+        })
+    }
+}
+
+export const getNewArrival = async (req, res) => {
+    try {
+        const newArrivals = await productModel.find().sort({ createdAt: -1 }).limit(8);
+        res.json(newArrivals);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server Error",
+            error: error.message
+        })
+    }
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { createProduct, deleteProduct, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getCategoryProduct, getHigherRatedProduct, getNewArrival, getProduct, getQueryProduct, updateProduct } from "../controllers/productController.js";
 import { admin, protect } from "../middlewares/authMiddleware.js";
 
 const productRouter = express.Router();
@@ -8,5 +8,16 @@ const productRouter = express.Router();
 productRouter.post("/", protect, admin, createProduct);
 productRouter.post("/:id", protect, admin, updateProduct);
 productRouter.delete("/:id", protect, admin, deleteProduct);
+//Get all the products with query parameters
+productRouter.get("/", getQueryProduct);
+//Get Products with Higher ratings
+productRouter.get("/best-seller", getHigherRatedProduct);
+//Get the latest 8 products which is created new acc to date
+productRouter.get("/new-arrival", getNewArrival);
+
+
+productRouter.get("/similar/:id", getCategoryProduct);
+productRouter.get("/:id", getProduct);
+
 
 export default productRouter;
